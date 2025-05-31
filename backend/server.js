a1559e5e5a7d329c263f315debd6a48173d059dc
chandraprakash goyal <155667417+chandraprakash22gyl@users.noreply.github.com>
2025-05-31 16:10:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,17 @@ import orderRouter from "./routes/orderRoute.js"
 const app = express()
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ['https://food-del-frontend-pe9v.onrender.com'];
+// const allowedOrigins = ['https://food-del-frontend-pe9v.onrender.com'];
 
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true, // if using cookies or tokens in headers
+// }))
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true, // if using cookies or tokens in headers
-}))
+  origin: ['https://food-del-admin-64xb.onrender.com', 'https://food-del-frontend-pe9v.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,  // if you need cookies/auth
+}));
 
 
 // middleware
