b01e578218177d042b4706650feae1b94fdfa1ba
chandraprakash goyal <155667417+chandraprakash22gyl@users.noreply.github.com>
2025-05-31 16:58:23 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
+import headerImage from '../../assets/header_img.png
 
 const Header = () => {
   return (
     <div className='header'>
-        <div className="header-contents">
+        <div className="header-contents" style={{ backgroundImage: `url(${headerImage})` }}>
             <h2>Order your favorite food here</h2>
             <p>Choose from a diverse menu featuring a delactable array of dishes crafted with the finest ingredients and satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
             <a href="#explore-menu"><button className='buttonwl'>View Menu</button></a>
