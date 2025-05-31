d37125211ee02a6ebebdb0258a949222688cdb43
chandraprakash goyal <155667417+chandraprakash22gyl@users.noreply.github.com>
2025-05-31 17:00:09 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import headerImage from '../../assets/header_img.png
+import headerImage from '../../assets/header_img.png'
 
 const Header = () => {
   return (
